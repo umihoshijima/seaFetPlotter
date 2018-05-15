00d41dbd0aabbd24dc47b65ee567a500ac46f586
Umihiko Hoshijima <umihoshijima@gmail.com>
2018-05-15 13:28:14 -0700

    altered and modified script

--- server.R
+++ server.R
@@ -4,7 +4,8 @@
 # http://shiny.rstudio.com
 #
 # library(rsconnect)
-# rsconnect::deployApp('~/Dropbox/SeaFET files/seaFetPlotter/')
+# run this next line without uncommennting it
+ # rsconnect::deployApp('~/Dropbox/SeaFET files/seaFetPlotter/')
 
 library(shiny)
 library(dplyr)
@@ -19,26 +20,30 @@ shinyServer(function(input, output) {
     inFile = input$file1
     if (is.null(inFile)) return (NULL)
     dat = read.csv(inFile$datapath, header=input$header, skip=input$skip, 
-                   sep = input$sep)
+                    sep = input$sep)
+    #get rid of NA columns
+    
+    dat = dat[, !apply(is.na(dat), 2, all)]
     if(ncol(dat) == 12){
-      
+
       #Some SeaFET models:
       colnames(dat) = c('time', 'V.batt', 'V.therm', 'V.FET.int', 'V.FET.ext',
                         'V.power.iso', 'T.controller', 'T.Durafet', 'pn', 'sn',
                         'pH.int', 'pH.ext')
     } else if(ncol(dat) == 11){
-      
+
       #Older seafet models:
       colnames(dat) = c('time', 'V.batt', 'V.therm', 'V.FET.int', 'V.FET.ext',
-                        'V.power.iso', 'T.Durafet', 'pn',
+                        'V.power.iso','T.controller', 'T.Durafet', 'pn',
                         'pH.int', 'pH.ext')
-    }  else if(ncol(dat) == 10){
-      
+    }  
+    else if(ncol(dat) == 10){
+
       #Older seafet models:
       colnames(dat) = c('time', 'V.batt', 'V.therm', 'V.FET.int', 'V.FET.ext',
                         'V.power.iso', 'T.controller', 'T.Durafet', 'pn',
                         'pH.int', 'pH.ext')
-      
+
     }else if (ncol(dat) == 25){
       years = floor(dat$V2/1000)
       # get the remainder of column 2 after dividing by 1000 - this is days of the year
@@ -55,7 +60,7 @@ shinyServer(function(input, output) {
       dat$V.therm = dat$V13
       dat$V.batt = dat$V14
     } else if (ncol(dat) == 16) {
-      
+
       #Some satlantics have 16. whoooo!
       years = floor(dat$V2/1000)
       # get the remainder of column 2 after dividing by 1000 - this is days of the year
@@ -63,7 +68,7 @@ shinyServer(function(input, output) {
       # Take years and days, comine them to form date. Column 3 is fractional hours, multiply by 60 to get minutes, multiply again by 60 to get seconds. Add to POSIXct output and save as new column.
       dat$ymd = as.POSIXct(paste(years, days), format = '%Y %j', tz = 'UTC') + dat$V3*60*60
       dat$time = round(dat$ymd, 'mins')
-      
+
       dat$pH.int = dat$V4
       dat$pH.ext = dat$V5
       dat$temp = dat$V6
@@ -71,12 +76,12 @@ shinyServer(function(input, output) {
       dat$V.FET.ext = dat$V8
       dat$V.therm = dat$V9
       dat$V.batt = dat$V10
-      
-      
+
+
     } else {
       #Satlantics should have 27.
       #let's just rename the ones we care about for now...
-      
+
       dat$time = dat$ymd
       dat$pH.int = dat$V4
       dat$pH.ext = dat$V5
@@ -86,7 +91,9 @@ shinyServer(function(input, output) {
       dat$V.therm = dat$V13
       dat$V.batt = dat$V14
     }
+    dat$time = lubridate::ymd_hms(dat$time, tz = 'UTC')
     return(dat)
+    
   })
   
   #shows the table on the first pane
@@ -95,7 +102,9 @@ shinyServer(function(input, output) {
   # Second pane: plot based on Skip. 
   output$batteryPlot = renderPlot({
     dat2 = dat()[input$skipBattery+1:nrow(dat()),]
-    plot(lubridate::ymd_hms(dat2$time),dat2$V.batt, type = 'l')
+    plot(lubridate::ymd_hms(dat2$time),dat2$V.batt, type = 'l',
+         main = input$plot_title_diff)
+    
   })
   # Second pane: range based on Skip. 
   output$battery_Range <- renderText({
@@ -108,18 +117,51 @@ shinyServer(function(input, output) {
   output$voltagePlot = renderPlot({
     par(mar=c(5.1,4.1,4.1,4.1))
     
-    plot(V.FET.int ~ ymd_hms(time), data = dat(), type = 'l')
+    plot(V.FET.int ~ ymd_hms(time), data = dat(), type = 'l', ylim = c(input$v_in_min, input$v_in_max),
+         main = input$plot_title_diff)
 
     par(new = TRUE)
     
     plot(V.FET.ext ~ ymd_hms(time), data = dat(), xaxt = 'n', yaxt = 'n', ylab = '', 
-         xlab = '', col = 'red', type = 'l')
+         xlab = '', col = 'red', type = 'l', ylim = c(input$v_out_min, input$v_out_max))
     axis(4, col = 'red')
     mtext('V.FET.ext', side = 4, line = 2)
   })
+  # DiffPlot
+  output$diffPlot = renderPlot({
+    par(mar=c(5.1,4.1,4.1,4.1))
+    a = dat()
+    dat_diff = (a$pH.int - a$pH.ext)
+    dat_diff = dat_diff - mean(dat_diff)
+    plot(dat_diff ~ ymd_hms(dat()$time), type = 'l', ylim = c(input$pH_diff_min, input$pH_diff_max),
+         main = input$plot_title_diff)
+  })
+  
+  output$Vint_Range <- renderText({
+    dat2 = dat()[input$skipBattery+1:nrow(dat()),]$V.FET.int
+    dat2 = dat2[complete.cases(dat2)]
+    range(dat2)
+  })
+  
+  output$Vout_Range <- renderText({
+    dat3 = dat()[input$skipBattery+1:nrow(dat()),]$V.FET.ext
+    dat3 = dat3[complete.cases(dat3)]
+    range(dat3)
+  })
+  
+  output$pH_diff_range <- renderText({
+    a = dat()
+    dat_diff = (a$pH.int - a$pH.ext)
+    dat_diff = dat_diff - mean(dat_diff)
+    range(dat_diff)
+  })
+  
+  output$title = renderText({
+    inFile = input$file1
+    basename(inFile$datapath)
+    
+  })
+  
 })
   
-  
-  
-
 
--- ui.R
+++ ui.R
@@ -8,20 +8,24 @@
 library(shiny)
 
 navbarPage('SeaFET QC',
-           tabPanel('Instructions',
-                    helpText('This is a...'
-                    )),
-           tabPanel('Import Data',
+           tabPanel('1. Instructions',
+                    helpText('This program will take in a SeaFET file and output graphs and diagnostics. Follow the above tabs in order - each one has its own set of directions.')),
+           tabPanel('2. Import Data',
                     sidebarLayout(
                       sidebarPanel(
+                        helpText('Upload SeaFET file using dialog below. '),
                         fileInput('file1', 'Choose SeaFET File' ),
+                        helpText('Are there column names (no for Martz)?'),
                         checkboxInput('header', 'Header', FALSE),
+                        helpText('Comma separated is for .csv (Satlantic), tab separated for Martz.'),
                         radioButtons('sep', 'Separator',
-                                     c(Comma=',',
-                                       Tab='\t'),
-                                     ','),
-                        numericInput('skip', 'Skip', 0,
-                                     min = 0, max = 21)
+                                     c(
+                                       Tab='\t', Comma=','),
+                                     '\t'),
+                        helpText('You should see the data on the right  of this page. Decrease this number until it breaks, then go back. generally 8 or 21 for Martz SeaFETs. '),
+                        numericInput('skip', 'Skip', 21,
+                                     min = 0, max = 21),
+                      textInput('plot_title_diff', 'Plot Titles', value = '')
                       ),
                       mainPanel(
                         tableOutput('contents')
@@ -29,7 +33,7 @@ navbarPage('SeaFET QC',
                     )
            ),
            
-           tabPanel('Battery Plot',
+           tabPanel('3. Battery Plot',
                     sidebarLayout(
                       sidebarPanel(
                         helpText('This is the number of lines skipped at the beginning. Use only if there are weird datea at the beginning'),
@@ -43,11 +47,41 @@ navbarPage('SeaFET QC',
                     )
            ),
            
-           tabPanel('Voltages (in/out)',
+           tabPanel('4. Voltages (in/out)',
                     sidebarLayout(
-                      sidebarPanel(),
+                      sidebarPanel(
+                        helpText('This is the voltages of the in/out electrodes.'),
+                        numericInput("v_in_min", "V_in min", value = .03, step = .01),
+                        numericInput("v_in_max", "V_in max", value = .1, step = .01),
+                        numericInput("v_out_min", "V_out min", value = -.95, step = .01),
+                        numericInput("v_out_max", "V_out max", value = -.8, step = .01), 
+                        helpText('V_int range (norm is .03 ~ .1):'),
+                        verbatimTextOutput('Vint_Range'), 
+                        helpText('V_out range (norm is -.95 ~ -.8):'),
+                        verbatimTextOutput('Vout_Range')
+                        
+                        
+                      ),
                       mainPanel(
                         plotOutput('voltagePlot')
+                        
+                      )
+                    )
+           ), 
+           tabPanel('5. pH difference',
+                    sidebarLayout(
+                      sidebarPanel(
+                        helpText('This is the difference in pH between the electrodes.'),
+                        numericInput("pH_diff_min", "pH diff min", value = -.02, step = .001),
+                        numericInput("pH_diff_max", "pH diff max", value = .02, step = .001),
+                        helpText('pH_diff range'),
+                        verbatimTextOutput('pH_diff_range')
+
+
+                      ),
+                      mainPanel(
+                        plotOutput('diffPlot')
+
                       )
                     )
            )
